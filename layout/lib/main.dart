252adeabbb88ce7f8141e33ad589c645d87f94ee
gurcks8989 <21700057@handong.edu>
2021-01-23 13:25:11 +0900

    push interactive

--- layout/lib/main.dart
+++ layout/lib/main.dart
@@ -38,11 +38,7 @@ class MyApp extends StatelessWidget {
             ),
           ),
           /*3*/
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41'),
+          FavoriteWidget(),
         ],
       ),
     );
@@ -83,7 +79,7 @@ class MyApp extends StatelessWidget {
               width: 600,
               height: 240,
               fit: BoxFit.cover,
-              ),
+            ),
             titleSection,
             buttonSection,
             textSection,
@@ -108,6 +104,52 @@ class MyApp extends StatelessWidget {
               fontWeight: FontWeight.w400,
               color: color,
             ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    void _toggleFavorite() {
+      setState(() {
+        if (_isFavorited) {
+          _favoriteCount -= 1;
+          _isFavorited = false;
+        } else {
+          _favoriteCount += 1;
+          _isFavorited = true;
+        }
+      });
+    }
+// A star icon and the number 41
+// A star_border icon and the number 40
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
           ),
         ),
       ],
